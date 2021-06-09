4938ae7d388495f454fe43139ce229fc07aa806e
Buryat312 <81243184+Buryat312@users.noreply.github.com>
2021-06-09 15:51:47 +0600

    Update lab_parser.py
    updated parser code, issues with category - fixed

--- lab_parser.py
+++ lab_parser.py
@@ -1,3 +1,4 @@
+from numpy import product
 import requests
 import csv
 from bs4 import BeautifulSoup
@@ -11,14 +12,10 @@ class Writer:
             'product_link': products.product_links,
             'category': products.categories}
         )
-    def write_to_xlsx(self):
-        writer = pd.ExcelWriter('products.xlsx', engine='xlsxwriter')
-        self.prodDF.to_excel(writer, sheet_name='Product', index=False)
-        writer.save()
 
     def write_to_csv(self):
         csvFileContents = self.prodDF.to_csv(index=False)
-        with open("products.csv", "w", encoding='utf-8') as f:
+        with open("products.csv", "a", encoding='utf-8') as f:
             f.write(csvFileContents)
 
 class Products:
@@ -35,12 +32,12 @@ class Products:
             self.product_links.append(prod.product_link)
             self.categories.append(prod.category)
 
-def writer(data):
-    with open('lab_kivano.csv', 'a') as file:
-        write = csv.writer(file)
-        return write.writerow((
-            data
-        ))
+# def writer(data):
+#     with open('lab_kivano.csv', 'a') as file:
+#         write = csv.writer(file)
+#         return write.writerow((
+#             data
+#         ))
 
 class Kivano:
     def __init__(self, product_name, product_link, category):
@@ -56,31 +53,34 @@ def get_data(url):
     soup = BeautifulSoup(html, 'html.parser')
     ads = soup.find('div', class_='list-view').find_all_next('div', class_='item')
     url = 'https://www.kivano.kg'
+    data = []
     for ad in ads:
         try:
             product_name = ad.find('div', class_='listbox_title').text
+            product_name = product_name.strip()
         except:
             product_name = 'Нет имени'
         try:
-            product_link = ad.find('div', class_='listbox_title').find('a').get('href')
+            product_link = url+ ad.find('div', class_='listbox_title').find('a').get('href')
         except:
             product_link = 'Нет линка'
         try:
-            category = soup.find('div', class_='product-index').find('div', class_='portlet-title').find('ul', class_='breadcrumb2').find('li', itemprop="itemListElement")[2].find('a').get('href')
-            category = category.strip()
+            category = soup.find('div', class_='product-index').find('div', class_='portlet-title').find('ul', class_='breadcrumb2').find_all('li', itemprop="itemListElement")[-1].text
         except:
             category = 'Другое'
         
+        data.append(Kivano(product_name, product_link, category))
         # data.append(product_name)
         # data.append(product_link)
         # data.append(category)
-        data = [
-            product_name,
-            url + product_link,
-            category,
-        ]
-        print(data)
-        writer(data)
+        # data = [
+        #     product_name,
+        #     url + product_link,
+        #     category,
+        # ]
+        # print(data)
+        # writer(data)
+    return data
 
 # exmpl = []
 def main():
@@ -100,9 +100,13 @@ def main():
             url_gen = url + page_part + str(i)
             # print(url_gen)   #ВЫТАСКИВАЕТ 3 СТР С КАЖДОГО САЙТА
             # print(get_data(url_gen))
-            get_data(url_gen)
+            # get_data(url_gen)
+            product = Products(get_data(url_gen))
+            writer = Writer(product)
             # exmpl.append(Kivano(data[0]), data[1], data[2])
             # print(exmpl)
+            writer.write_to_csv()
+
 
 
 if __name__ == '__main__':
